b43c2249ad276972a751d4d9793f8fdd77e9c03b
Jaron <929770492@qq.com>
2024-09-12 10:50:31 +0800

    9/12

---
+++ practice.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+import altair as alt
+import streamlit as st
+
+st.header("Practice Streamlit")
+
+st.write("Hello, *World!* :sunglasses:")
+
+# st.header("st.button")
+#
+# if st.button("Say hello"):
+#     st.write("Why hello there")
+# else:
+#     st.write("Goodbye")
+
+st.write(1234)
+
+df = pd.DataFrame({
+    "first_column": [1, 2, 3, 4],
+    "second_column": [10, 20, 30, 40]
+})
+
+st.write(df)
+
+st.write("Below is a DataFrame:", df, "above is a dataframe")
+
+df2 = pd.DataFrame(
+    np.random.randn(200, 3), #200行3列的随机数据
+    columns=['a', 'b', 'c'])
+
+c=alt.Chart(df2).mark_circle().encode(
+    x='a',y='b',size='c',color='c',tooltip=['a','b','c'])
+
+st.write(c)
